8f391f6a0c3c740d46abea1ffdea0c48cfc497c1
Raph <contact@raphael-jolivet.name>
2019-02-15 02:30:47 +0100

    Working on integration tests

--- src/test/integrated/rest/add-db.test.ts
+++ src/test/integrated/rest/add-db.test.ts
@@ -13,25 +13,31 @@ let server : Server  = null;
 let client_path = path.resolve(__dirname, "..", "..", "..", "..", "dist", "client");
 let server_path = path.resolve(__dirname, "..", "..", "..", "..", "dist", "server");
 
-beforeAll(() => {
+beforeAll(done => {
 
     let initServer = serverModule.default;
 
     // Create in memory DB
     mongod = new MongoMemoryServer({instance:{port: DB_PORT}});
     config.DB_PORT = DB_PORT;
+
     let exp = initServer([client_path, server_path]);
-    server = exp.listen(SERVER_PORT, () => {
-        console.log("Server started on port : ", SERVER_PORT);
-        setTimeout(() => {
-            console.log("Closing server");
-            server.close();
-        }, 10000);
+
+    mongod.getConnectionString().then(() => {
+        console.log("Mongo started on port : ", DB_PORT);
+        server = exp.listen(SERVER_PORT, () => {
+            console.log("Server started on port : ", SERVER_PORT);
+            done();
+        });
     });
+
+
 
 }, 30000);
 
 
-test("wait !", () => {
-
-});
+test("wait !", done => {
+    setTimeout(() => {
+        done();
+    }, 300000);
+}, 300000);
